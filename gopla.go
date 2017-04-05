91047ed0f070b2e31426fc30a8f069794d9bc922
ecdsa521 <ecdsa521@gmail.com>
2017-04-05 14:15:49 +0200

    Add options for mass downloads and searching

--- gopla.go
+++ gopla.go
@@ -1,8 +1,10 @@
 package gopla
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"regexp"
 	"strconv"
 
@@ -34,6 +36,20 @@ type VideoData struct {
 	Videos      VideoStreams
 }
 
+//SearchData has all information from search page
+type SearchData struct {
+	Title       string
+	Description string
+	ID          string
+	Date        string
+	Image       string
+	URL         string
+	Hash        string
+}
+
+//SearchDatas ...
+type SearchDatas []SearchData
+
 //VideoDatas ..
 type VideoDatas []VideoData
 
@@ -52,6 +68,61 @@ func (slice VideoStreams) Less(i, j int) bool {
 
 func (slice VideoStreams) Swap(i, j int) {
 	slice[i], slice[j] = slice[j], slice[i]
+}
+
+func getJSON(url string) gjson.Result {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla")
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	json, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	var jsonR = gjson.Parse(string(json))
+	return jsonR
+}
+
+//GetAllHashes returns all video hashes from given search
+func GetAllHashes(id string, name string) []string {
+	var link = fmt.Sprintf("https://seeker.redefine.pl/ipla/multi.json?free=1&page=1&platform=www_ipla_tv&portal_id=ipla&query=%s&category_ids%%5B%%5D=%s&size=5000&sort=recent&sound=&top_category=", url.QueryEscape(name), url.QueryEscape(id))
+	var jsonR = getJSON(link)
+	var data []string
+	for _, v := range jsonR.Array() {
+		data = append(data, v.Get("media_id").String())
+	}
+	return data
+}
+
+//FindVideo finds the material and returns informations about it
+func FindVideo(name string) SearchDatas {
+	var link = fmt.Sprintf("https://seeker.redefine.pl/ipla/multi.json?free=1&page=1&platform=www_ipla_tv&portal_id=ipla&query=%s&size=150&sort=recent&sound=&top_category=KATEGORIE", url.QueryEscape(name))
+	var jsonR = getJSON(link)
+
+	if len(jsonR.Array()) == 0 {
+		return nil
+	}
+	var data SearchDatas
+	for _, v := range jsonR.Array() {
+		data = append(data, SearchData{
+			ID:          v.Get("id").String(),
+			Description: v.Get("description").String(),
+			Title:       v.Get("title").String(),
+			Date:        v.Get("created_date").String(),
+			Image:       v.Get("image").String(),
+			URL:         fmt.Sprintf("http://www.ipla.tv/kategoria/%s", v.Get("id").String()),
+			Hash:        v.Get("media_id").String(),
+		})
+	}
+	return data
 }
 
 //GetHash returns playvod| link from http link
@@ -76,24 +147,7 @@ func GetHash(link string) string {
 //GetVideo returns VideoStream object with basic link informations
 func GetVideo(hash string) VideoData {
 
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", "http://getmedia.redefine.pl/vods/get_vod/?cpid=1&ua=mipla/23&media_id="+hash, nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("User-Agent", "Mozilla")
-	res, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-	json, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
-	var jsonR = gjson.Parse(string(json))
-
+	var jsonR = getJSON("http://getmedia.redefine.pl/vods/get_vod/?cpid=1&ua=mipla/23&media_id=" + hash)
 	var data = VideoData{
 		Hash:        hash,
 		Title:       jsonR.Get("vod.title").String(),
--- gopla/main.go
+++ gopla/main.go
@@ -12,10 +12,65 @@ import (
 
 var verbose = flag.Bool("v", false, "Verbose operation")
 var best = flag.Bool("b", false, "Only print best option")
+var search = flag.String("s", "", "Search for this show")
+var grabHashes = flag.Bool("a", false, "Grab all hashes for search query")
+var grabLinks = flag.Bool("l", false, "Grab all links for search query")
+var wgetLinks = flag.Bool("w", false, "Generate batch output for wget downloads")
+
+func wgetAllFiles(data *gopla.SearchData) {
+	for _, v := range gopla.GetAllHashes(data.ID, data.Title) {
+		var data = gopla.GetVideo(v)
+		sort.Sort(sort.Reverse(&data.Videos))
+		for i, d := range data.Videos {
+			if *best {
+				fmt.Printf("wget -O '%s.mp4' '%s'\n", data.Title, d.URL)
+				break
+			} else {
+				fmt.Printf("wget -O '%s_%d.mp4' '%s'\n", data.Title, i, d.URL)
+			}
+
+		}
+	}
+}
+func grabAllHashes(data *gopla.SearchData) {
+	for _, v := range gopla.GetAllHashes(data.ID, data.Title) {
+		color.Blue("%s", v)
+	}
+}
+
+func grabAllLinks(data *gopla.SearchData) {
+	for _, v := range gopla.GetAllHashes(data.ID, data.Title) {
+		grabByHash(v)
+	}
+}
+func grabByName(name string) {
+	var res = gopla.FindVideo(name)
+	for _, v := range res {
+		if *verbose {
+			color.Green(v.Title)
+			color.Cyan(v.Description)
+			color.Blue(v.URL)
+
+		}
+		if *grabHashes {
+			grabAllHashes(&v)
+		}
+		if *grabLinks {
+			grabAllLinks(&v)
+		}
+		if *wgetLinks {
+			wgetAllFiles(&v)
+		}
+	}
+}
 
 func grabByLink(link string) {
 	var hash = gopla.GetHash(link)
+	if *verbose {
+		color.Cyan("playvod:%s", hash)
+	}
 	grabByHash(hash)
+
 }
 
 func grabByHash(hash string) {
@@ -41,14 +96,17 @@ func grabByHash(hash string) {
 func main() {
 
 	flag.Parse()
-	for _, link := range flag.Args() {
+	if len(*search) > 0 {
+		grabByName(*search)
+	} else {
+		for _, link := range flag.Args() {
 
-		if match, _ := regexp.MatchString("[a-f0-9]{16}", link); match == true {
-			grabByHash(link)
-		}
-		if match, _ := regexp.MatchString("www.ipla.tv", link); match == true {
-			grabByLink(link)
+			if match, _ := regexp.MatchString("[a-f0-9]+", link); match == true {
+				grabByHash(link)
+			}
+			if match, _ := regexp.MatchString("www.ipla.tv", link); match == true {
+				grabByLink(link)
+			}
 		}
 	}
-
 }
